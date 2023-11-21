02510e60fd9eeb11d61ebbddc72725b1ebd46fd0
giannismon <mongiannis@gmail.com>
2023-11-21 22:58:39 +0200

    ---

--- pipeline.groovy
+++ pipeline.groovy
@@ -21,7 +21,7 @@ pipeline {
                     echo "Waiting ${waitTime} seconds for the server to become available..."
                     sleep(waitTime)
 
-                    def serverStatus = sh(script: 'ping -c 1 your_server_ip_or_hostname', returnStatus: true)
+                    def serverStatus = sh(script: 'ping -c 1 192.168.2.11', returnStatus: true)
 
                     if (serverStatus == 0) {
                         echo 'Server is reachable. Proceeding with the next steps.'
