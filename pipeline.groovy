41bf0473e8025e1e3aedb4752200370ff6b5764b
giannismon <mongiannis@gmail.com>
2023-11-21 22:37:29 +0200

    ---

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,9 +2,38 @@ pipeline {
     agent any
 
     stages {
-        stage('Print Message') {
+        stage('Reboot and Check Server') {
             steps {
-                echo 'Hello, this is a simple Jenkins Pipeline!'
+                // Βήμα 1: Επανεκκίνηση του server
+                script {
+                    sh 'sudo reboot now'
+                }
+            }
+        }
+
+        stage('Wait for Server to Open') {
+            steps {
+                // Βήμα 2: Αναμονή για τον έλεγχο της διαθεσιμότητας του server
+                script {
+                    def waitTime = 300
+                    echo "Waiting ${waitTime} seconds for the server to become available..."
+                    sleep(waitTime)
+
+                    def serverStatus = sh(script: 'ping -c 1 192.168.2.11', returnStatus: true)
+
+                    if (serverStatus == 0) {
+                        echo 'Server is reachable. Proceeding with the next steps.'
+                    } else {
+                        error 'Server is not reachable. Pipeline will be marked as failed.'
+                    }
+                }
+            }
+        }
+
+        stage('Additional Steps') {
+            steps {
+                // Βήμα 3: Εκτέλεση επιπλέον βημάτων αν ο έλεγχος είναι επιτυχής
+                echo 'Additional steps after the server is reachable...'
             }
         }
     }
