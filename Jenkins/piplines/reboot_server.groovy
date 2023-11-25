02e94fb5a1c524b1f103e4d9866f8c45a81731ab
giannismon <mongiannis@gmail.com>
2023-11-25 21:13:03 +0200

    ---

--- Jenkins/piplines/reboot_server.groovy
+++ Jenkins/piplines/reboot_server.groovy
@@ -2,52 +2,27 @@ pipeline {
     agent any
 
     stages {
-        stage('Reboot and Check Server') {
+        stage('Reboot Server') {
             steps {
-                // Βήμα 1: Επανεκκίνηση του server
                 script {
+                    // Εκτελούμε την εντολή reboot με sudo και τον χρήστη root
+                    sh 'echo "root_password" | sudo -S reboot'
+                    
+                    // Περιμένουμε 1 λεπτό (60 δευτερόλεπτα)
+                    sleep 60
+                }
+            }
+        }
+        
+        stage('Check Server Status') {
+            steps {
+                script {
+                    // Εδώ μπορείτε να προσθέσετε εντολές για να ελέγξετε την κατάσταση του server
+                    // π.χ., να προσπαθήσετε να συνδεθείτε ξανά, να ελέγξετε το uptime, κλπ.
+                    // Το παράδειγμα χρησιμοποιεί έναν υποθετικό έλεγχο του uptime.
                     sh 'uptime'
-                    sh 'hostname'
-                    sh 'sudo reboot now > /dev/null 2>&1 &'
-                    sleep(time: 10, unit: 'SECONDS')
-
                 }
             }
-        }
-
-        stage('Wait for Server to Open') {
-            steps {
-                // Βήμα 2: Αναμονή για τον έλεγχο της διαθεσιμότητας του server
-                script {
-                    def waitTime = 50
-                    echo "Waiting ${waitTime} seconds for the server to become available..."
-                    sleep(waitTime)
-
-                    def serverStatus = sh(script: 'ping -c 1 192.168.2.11', returnStatus: true)
-
-                    if (serverStatus == 0) {
-                        echo 'Server is reachable. Proceeding with the next steps.'
-                    } else {
-                        error 'Server is not reachable. Pipeline will be marked as failed.'
-                    }
-                }
-            }
-        }
-
-        stage('Additional Steps') {
-            steps {
-                // Βήμα 3: Εκτέλεση επιπλέον βημάτων αν ο έλεγχος είναι επιτυχής
-                echo 'Additional steps after the server is reachable...'
-            }
-        }
-    }
-
-    post {
-        success {
-            echo 'Pipeline succeeded!'
-        }
-        failure {
-            echo 'Pipeline failed!'
         }
     }
 }
