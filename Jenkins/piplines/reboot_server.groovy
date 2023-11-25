944c0326e29e8295b17a685e90e7ddbbe9ce64b4
giannismon <mongiannis@gmail.com>
2023-11-26 01:14:51 +0200

    ---

--- Jenkins/piplines/reboot_server.groovy
+++ Jenkins/piplines/reboot_server.groovy
@@ -1,51 +1,21 @@
 pipeline {
     agent any
 
-    stages {
-        stage('Reboot and Check Server') {
-            steps {
-                // Βήμα 1: Επανεκκίνηση του server
-                script {
-                    sh 'nohup sudo reboot now > /dev/null 2>&1 &'
-                    sleep(time: 10, unit: 'SECONDS')
-
-                }
-            }
-        }
-
-        stage('Wait for Server to Open') {
-            steps {
-                // Βήμα 2: Αναμονή για τον έλεγχο της διαθεσιμότητας του server
-                script {
-                    def waitTime = 50
-                    echo "Waiting ${waitTime} seconds for the server to become available..."
-                    sleep(waitTime)
-
-                    def serverStatus = sh(script: 'ping -c 1 192.168.2.11', returnStatus: true)
-
-                    if (serverStatus == 0) {
-                        echo 'Server is reachable. Proceeding with the next steps.'
-                    } else {
-                        error 'Server is not reachable. Pipeline will be marked as failed.'
-                    }
-                }
-            }
-        }
-
-        stage('Additional Steps') {
-            steps {
-                // Βήμα 3: Εκτέλεση επιπλέον βημάτων αν ο έλεγχος είναι επιτυχής
-                echo 'Additional steps after the server is reachable...'
-            }
-        }
+    environment {
+        // Ορίστε τη διεύθυνση του Git repository
+        GIT_REPO_URL = 'https://github.com/giannismon/local.git'
     }
 
-    post {
-        success {
-            echo 'Pipeline succeeded!'
+    stages {
+        stage('Clone Repository') {
+            steps {
+                // Κάνε clone το Git repository
+                script {
+                    sh "git clone ${GIT_REPO_URL}"
+                }
+            }
         }
-        failure {
-            echo 'Pipeline failed!'
-        }
+
+        // Εδώ μπορείτε να προσθέσετε επιπλέον stages για το build, το deploy, κλπ.
     }
 }
