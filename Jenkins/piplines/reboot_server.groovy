5379e7e82492689e86b7b68fd5990ca654c5d4a0
giannismon <mongiannis@gmail.com>
2023-11-26 17:24:32 +0200

    ----

--- Jenkins/piplines/reboot_server.groovy
+++ Jenkins/piplines/reboot_server.groovy
@@ -8,7 +8,7 @@ pipeline {
                     // Συνδέσου στον server και κάνε restart
 
                     sh 'hostname'
-                    sh 'nohup sudo restart -r now > /dev/null 2>&1 &'
+                    sh 'nohup sudo reboot > /dev/null 2>&1 &'
 
                     // Περίμενε 20 λεπτά πριν συνεχίσεις
                     sh 'sleep 20'
